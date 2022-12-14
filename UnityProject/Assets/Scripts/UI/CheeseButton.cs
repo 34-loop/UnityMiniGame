8c34852a725f56378056f9419386d3378be17846
Igor-Fursov <84546479+Igor-Fursov@users.noreply.github.com>
2022-12-14 19:27:32 +0300

    добавлен скрипт кнопки
    -добавлен скрипт кнопки CheeseButton.

---
+++ UnityProject/Assets/Scripts/UI/CheeseButton.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CheeseButton : MonoBehaviour
+{
+    private Button _cheeseButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var root = GetComponent<UIDocument>().rootVisualElement;
+        _cheeseButton = root.Q<Button>("cheese-button");
+        _cheeseButton.clicked += OnCheeseButtonClicked;
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCheeseButtonClicked() 
+    {
+        Debug.Log("Cheese activated");  
+ 
+    }
+
+
+
+
+}
